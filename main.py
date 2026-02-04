20c9efd4513f3b5872b47aca6a61df3b76d45b5d
BlazeXMusic <tijilkumar7@gmail.com>
2026-02-04 23:22:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import time,os,platform
-try:from prettytable import PrettyTable
+try:
+	from prettytable import PrettyTable
 except:os.system("pip install prettytable")
 rd, gn, lgn, yw, lrd, be, pe = '\033[00;31m', '\033[00;32m', '\033[01;32m', '\033[01;33m', '\033[01;31m', '\033[94m', '\033[01;35m'
 cn, k,g = '\033[00;36m', '\033[90m','\033[38;5;130m'
